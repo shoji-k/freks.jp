2674a3f548bb70f41cccf8db7125824b2e950ea2
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-10-19 21:00:22 +0900

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-// 必要なプラグインを読み込む
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// Sassをコンパイルするタスク設定
-gulp.task("default", function () {
+gulp.task("compile", function () {
     gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(gulp.dest('css/'));
 });
+
+gulp.task("watch", function() {
+  gulp.watch('./node_modules/bootstrap/scss/*.scss', ['compile']);
+});
+
+gulp.task("default", ['compile']);
