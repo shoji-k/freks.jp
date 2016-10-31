4ec6cb5c68ef07f0a16564b1af77aea70e8de711
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-10-31 16:33:32 +0900

    set floating label to form

--- js/main.js
+++ js/main.js
@@ -19,4 +19,14 @@
     $('.navbar-toggler:visible').click();
   });
 
+  $(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
+
 })(jQuery);
