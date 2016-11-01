1fac047ca23482e7ca2f854ae7675c8753ce5099
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-11-01 19:32:49 +0900

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 
 gulp.task("compile", function () {
     gulp.src('./scss/*.scss')
@@ -11,6 +13,13 @@ gulp.task("compile", function () {
 
 gulp.task("watch", function() {
   gulp.watch('./scss/*.scss', ['compile']);
+});
+
+gulp.task('minify-css', ['compile'], function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({ compatibility: '*' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task("default", ['compile']);
