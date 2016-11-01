cb361d9d70bf31b3a26abce3f911cd30fd2a96ff
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-11-01 17:23:53 +0900

    prepare contact form

--- js/contact.js
+++ js/contact.js
@@ -1,93 +1,90 @@
 $(function() {
-  var mail = {
-    preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
+  var m = {
+    showResult: function(message) {
+      $('#result').html("<div class='alert alert-success'>");
+      $('#result > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+      $('#result > .alert-success')
+        .append("<strong>"+message+"</strong>");
+      $('#result > .alert-success')
+        .append('</div>');
     },
-    submitSuccess: function($form, event) {
-      // Prevent spam click and default submit behaviour
-      $("#btnSubmit").attr("disabled", true);
-      event.preventDefault();
-
-      // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
-      $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
-        cache: false,
-        success: function() {
-          $("#btnSubmit").attr("disabled", false);
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-
-          $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-      });
-    },
-    filter: function() {
-      return $(this).is(":visible");
-    },
+    showError: function(message) {
+      $('#result').html("<div class='alert alert-danger'>");
+      $('#result > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#result > .alert-danger').append("<strong>"+message+"</strong>");
+      $('#result > .alert-danger').append('</div>');
+    }
   };
-
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-
   $('#submitBtn').on('click', function() {
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
+
+      var error = false;
+      if (! name) {
+          $("#nameError").html('名前を入力してください');
+          error = true;
+      }
+      if (! email) {
+          $("#emailError").html('メールアドレスを入力してください');
+          error = true;
+      }
+      if (! message) {
+          $("#messageError").html('メッセージを入力してください');
+          error = true;
+      }
+      if (error) {
+          return false;
+      }
+
+      $("#submitBtn").attr("disabled", true);
+      $("#submitBtn").html("<span class=\"loader\"></span>Sending..");
+      $("#result").html('');
 
       $.ajax({
         url: "../mail/contact.php",
         type: "POST",
         data: {
           name: name,
-          phone: phone,
           email: email,
           message: message
         },
         cache: false,
-        success: function() {
-          console.log('success');
+        success: function(res) {
+          $("#submitBtn").attr("disabled", false);
+
+          if (res.result) {
+            m.showResult(res.message);
+          } else {
+            m.showError(res.message);
+          }
+
+          $("#submitBtn").html("Send");
+          $('#contactForm').trigger("reset");
         },
         error: function() {
-          console.log('fail');
+          $("#submitBtn").attr("disabled", false);
+
+          m.showError("エラーが発生しました<br>contact"+"@"+"freks.jpまでご連絡ください");
+
+          $("#submitBtn").html("Send");
+          $('#contactForm').trigger("reset");
         },
       });
   });
+
+  $('#name').focus(function() {
+    $('#result').html('');
+    $("#nameError").html('');
+  });
+  $('#email').focus(function() {
+    $('#result').html('');
+    $("#emailError").html('');
+  });
+  $('#message').focus(function() {
+    $('#result').html('');
+    $("#messageError").html('');
+  });
 });
 
-$('#name').focus(function() {
-  $('#success').html('');
-});
--- mail/contact.php
+++ mail/contact.php
@@ -1,24 +1,34 @@
 <?php
+$r['result'] = false;
+
 // validate
 if(empty($_POST['name'])  ||
     empty($_POST['email']) ||
-    empty($_POST['phone']) ||
-    empty($_POST['message']) ||
-    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+    empty($_POST['message']))
+    // || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
 {
-    echo "No arguments Provided!";
+    $r['message'] = "フォームに未入力の項目があります";
+    header("Content-Type: application/json; charset=utf-8");
+    echo json_encode($r);
     return false;
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = 'contact+webpagecontact@freks.jp';
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: contact@freks.jp\n";
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+if (mail($to,$email_subject,$email_body,$headers)) {
+    $r['result'] = true;
+    $r['message'] = "ご連絡ありがとうございました";
+} else {
+    $r['message'] = "エラーが発生しました<br>contact@freks.jpまでご連絡ください";
+}
+
+header("Content-Type: application/json; charset=utf-8");
+echo json_encode($r);
 return true;
