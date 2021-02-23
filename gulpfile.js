e8a946a8ebbee40796be19ded628a9a76689f835
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-02-23 11:17:23 +0900

    brush up js gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel } = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
-// var uglify = require("gulp-uglify");
+const uglify = require("gulp-uglify");
 // var concat = require("gulp-concat");
 // var gzip = require("gulp-gzip");
 
@@ -21,23 +21,18 @@ function minifyCss() {
     .pipe(dest("css"));
 }
 
+function minifyJs() {
+  return src('js/script.js')
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('js'));
+}
+
 // gulp.task("watch", function() {
 //   gulp.watch('./scss/*.scss', ['sass']);
 //   gulp.watch('./js/*.js', ['concat-js']);
 // });
 
-// gulp.task('concat-js', function() {
-//   return gulp.src(['js/main.js','js/contact.js'])
-//     .pipe(concat('script.js'))
-//     .pipe(gulp.dest('js'));
-// });
-//
-// gulp.task('minify-js', function() {
-//   return gulp.src('js/script.js')
-//     .pipe(uglify())
-//     .pipe(rename({ suffix: '.min' }))
-//     .pipe(gulp.dest('js'));
-// });
 // gulp.task('compress-js', function() {
 //   return gulp.src('js/script.min.js')
 //     .pipe(gzip())
@@ -48,10 +43,5 @@ function minifyCss() {
 //     .pipe(gzip())
 //     .pipe(gulp.dest('css'));
 // });
-//
-// gulp.task("initial", ['copy', 'compile']);
-// gulp.task("compile", ['sass', 'concat-js']);
-// gulp.task("release", ['minify-css', 'minify-js', 'compress-css', 'compress-js']);
-// gulp.task("default", gulp.task('sass'));
 
-exports.default = series(compileCss, minifyCss);
+exports.default = parallel(series(compileCss, minifyCss), minifyJs);
